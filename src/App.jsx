1a77073867b4f64211016ac5c5adc98ca6ab1420
h-matsushita14 <h.matsushita14@gmail.com>
2025-09-06 20:24:01 +0900

    fix(app): Fix header overlap and app crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { Toolbar } from '@mui/material';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
