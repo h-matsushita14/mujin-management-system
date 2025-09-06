138ecc4b06e10c7de028c3b7a072b88e52412e12
h.matsushita <h.matsushita14@gmail.com>
2025-09-06 17:04:48 +0900

    feat: Change ReportingPage title to "レジ締め報告"\n\nUpdated the title of the ReportingPage from "報告機能" to "レジ締め報告" for clarity and better user understanding.

--- src/pages/ReportingPage.jsx
+++ src/pages/ReportingPage.jsx
@@ -4,7 +4,7 @@ function ReportingPage() {
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
-        報告機能
+        レジ締め報告
       </Typography>
       <Typography variant="body1">
         日々のレジ締め業務後の報告機能。報告後に販売実績が在庫へ反映するページです。
